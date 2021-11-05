2cc95348c68e533ee2a3b0fc5807ebb5719a3949
Yahya Cherkaoui El Moqadem <yahya.cherkaouielmoqadem@hotmail.com>
2021-11-05 14:55:52 +0100

    Updated method SetCulture() to return language code according to given language.

--- P2FixAnAppDotNetCode/Models/Services/LanguageService.cs
+++ P2FixAnAppDotNetCode/Models/Services/LanguageService.cs
@@ -25,6 +25,18 @@ namespace P2FixAnAppDotNetCode.Models.Services
             string culture = "";
             // TODO complete the code 
             // Default language is "en", french is "fr" and spanish is "es".
+            switch(language)
+            {
+                case "English":
+                    culture = "en";
+                    break;
+                case "French":
+                    culture = "fr";
+                    break;
+                case "Spanish":
+                    culture = "es";
+                    break;
+            }
             
             return culture;
         }
